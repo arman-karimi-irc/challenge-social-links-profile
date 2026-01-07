bb10bdde77d73237d98f9d5d41ee499f8ef41235
iirccopilot <iirccopilot@gmail.com>
2026-01-07 12:24:57 +0330

    init profile

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,3 +1,14 @@
 export default function Profile() {
-    return <></>
+  return (
+    <div className="">
+      <img
+        className="rounded-full"
+        src="/public/assets/images/avatar-jessica.jpeg"
+        alt="avatar"
+      />
+      <h2 className="font-inter text-2xl">Jessica Randall</h2>
+      <b className="text-green">London, United Kingdom</b>
+      <p className="">"Front-end developer and avid reader."</p>
+    </div>
+  );
 }
